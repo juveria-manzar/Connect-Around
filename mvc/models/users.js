370f61dd36a4de60b099ddf17a3c526e991ff4e6
Juveria Manzar <juveriamanzar29@gmail.com>
2020-06-10 22:42:42 +0530

    Initial-Setup-Node

---
+++ mvc/models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto')
+
+const salt = crypto.randomBytes(64).toString('hex')
+
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: String,
+    salt: String,
+})
+
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(64).toString('hex');
+    this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+}
+userSchema.methods.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+    return hash === this.password;
+}
+
+mongoose.model('User', userSchema);
---
+++ mvc/routes/users.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/register', function({ body }, res, next) {
+    if (!Object.values(body).every((val) => val)) {
+        return res.send({ message: "All fields are required." })
+    }
+
+    if (body.password !== body.password_confirm) {
+        return res.send({ message: "Passwords don't match." })
+    }
+
+    const user = new User();
+    user.firstname = body.first_name.trim();
+    user.lastname = body.last_name.trim();
+    user.email = body.email.trim();
+    user.setPassword(body.password);
+
+    user.save((err, newUser) => {
+        if (err) {
+            res.status(404).json(err)
+        } else {
+            res.status(201).json({ message: "Created User", user: newUser })
+        }
+    })
+})
+
+router.post('/login', function(req, res) {
+    if (!req.email || req.password) {
+        return res.status(400).json({ message: "All fields are required" })
+    }
+
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            return res.status(404).json(err)
+        }
+        if (user) {
+            return res.status(201).json({ message: "Logged In." })
+        } else {
+            return res.status(401).json(info)
+        }
+    })(req, res)
+})
+
+module.exports = router;
