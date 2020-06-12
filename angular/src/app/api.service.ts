4334956d59cffa89cf9aeac6d4f698c8b053bd18
Juveria Manzar <juveriamanzar29@gmail.com>
2020-06-12 22:53:33 +0530

    api-service

---
+++ angular/src/app/api.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  private baseUrl='localhost:3000'
+
+  private successHandler(value){
+    return value;
+  }
+  private errorHandler(error){
+    return error;
+  }
+
+  public makeRequest(requestObject):any{
+    let type=requestObject.type.toLowerCase()
+    if(!type){
+      return console.log('No type specified in the request object')
+    }
+
+    let body=requestObject.body||{}
+    let location=requestObject.location
+    if(!location){
+      return console.log('No location specified in the request object')
+    }
+
+    let url=`${this.baseUrl}/${location}`
+
+    let httpOptions={}
+    if(type==='get'){
+      return this.http.get(url,httpOptions).toPromise()
+      .then(this.successHandler)
+      .catch(this.errorHandler)
+    }
+
+    if(type==='post'){
+      return this.http.post(url,body, httpOptions).toPromise()
+      .then(this.successHandler)
+      .catch(this.errorHandler)
+    }
+    console.log('Could not make request. Make sure type of GET or POST is supplied')
+  }
+}
